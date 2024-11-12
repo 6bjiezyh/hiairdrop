b7497dc24b161938e2c670741fd5ed15687dd44c
6bjiezyh <571962631@qq.com>
2024-11-12 16:00:54 +0800

    first commit

--- docker/openssl/create.sh
+++ docker/openssl/create.sh
@@ -2,8 +2,8 @@
 
 cnf_dir='/mnt/openssl/'
 certs_dir='/etc/ssl/certs/'
-openssl req -config ${cnf_dir}snapdropCA.cnf -new -x509 -days 1 -keyout ${certs_dir}snapdropCA.key -out ${certs_dir}snapdropCA.crt
-openssl req -config ${cnf_dir}snapdropCert.cnf -new -out /tmp/snapdrop-dev.csr -keyout ${certs_dir}snapdrop-dev.key
-openssl x509 -req -in /tmp/snapdrop-dev.csr -CA ${certs_dir}snapdropCA.crt -CAkey ${certs_dir}snapdropCA.key -CAcreateserial -extensions req_ext -extfile ${cnf_dir}snapdropCert.cnf -sha512 -days 1 -out ${certs_dir}snapdrop-dev.crt
+openssl req -config ${cnf_dir}hiAirDropCA.cnf -new -x509 -days 1 -keyout ${certs_dir}hiAirDropCA.key -out ${certs_dir}hiAirDropCA.crt
+openssl req -config ${cnf_dir}hiAirDropCert.cnf -new -out /tmp/hiAirDrop-dev.csr -keyout ${certs_dir}hiAirDrop-dev.key
+openssl x509 -req -in /tmp/hiAirDrop-dev.csr -CA ${certs_dir}hiAirDropCA.crt -CAkey ${certs_dir}hiAirDropCA.key -CAcreateserial -extensions req_ext -extfile ${cnf_dir}hiAirDropCert.cnf -sha512 -days 1 -out ${certs_dir}hiAirDrop-dev.crt
 
 exec "$@"
